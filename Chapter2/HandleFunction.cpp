cfabd191e5c4459437ee8673984cefc437b9c684
Panda98 <810650217@qq.com>
2018-09-23 16:05:06 +0800

    修改Function识别的参数识别；添加了公共函数ParseStatement

--- Chapter2/HandleFunction.cpp
+++ Chapter2/HandleFunction.cpp
@@ -1,54 +1,60 @@
 #pragma once
 #include "Global.h"
 /*******************
-*                  *
-** 处理function **
-*                  *
-********************/
+ *                  *
+ ** 处理function **
+ *                  *
+ ********************/
 /// prototype
 ///   ::= id '(' id* ')'
 std::unique_ptr<PrototypeAST> ParsePrototype() {
-	if (CurTok != VARIABLE)
-		return LogErrorP("Expected function name in prototype");
+  if (CurTok != VARIABLE)
+    return LogErrorP("Expected function name in prototype");
 
-	std::string FnName = IdentifierStr;
-	getNextToken();
+  std::string FnName = IdentifierStr;
+  getNextToken();
 
-	if (CurTok != '(')
-		return LogErrorP("Expected '(' in prototype");
+  if (CurTok != '(')
+    return LogErrorP("Expected '(' in prototype");
 
-	std::vector<std::string> ArgNames;
-	while (getNextToken() == VARIABLE)
-		ArgNames.push_back(IdentifierStr);
-	if (CurTok != ')')
-		return LogErrorP("Expected ')' in prototype");
+  std::vector<std::string> ArgNames;
+  getNextToken();
+  while (CurTok == VARIABLE) {
+	ArgNames.push_back(IdentifierStr);
+    getNextToken(); 
+	if (CurTok == ',')
+      getNextToken();
+  }
 
-	// success.
-	getNextToken(); // eat ')'.
+  if (CurTok != ')')
+    return LogErrorP("Expected ')' in prototype");
 
-	return llvm::make_unique<PrototypeAST>(FnName, std::move(ArgNames));
+  // success.
+  getNextToken(); // eat ')'.
+
+  return llvm::make_unique<PrototypeAST>(FnName, std::move(ArgNames));
 }
 
 /// definition ::= 'def' prototype expression
 std::unique_ptr<FunctionAST> ParseDefinition() {
-	getNextToken(); // eat def.
-	auto Proto = ParsePrototype();
-	if (!Proto)
-		return nullptr;
+  getNextToken(); // eat def.
+  auto Proto = ParsePrototype();
+  if (!Proto)
+    return nullptr;
 
-	if (auto E = ParseExpression())
-		return llvm::make_unique<FunctionAST>(std::move(Proto), std::move(E));
-	//TODO: 这个返回只是测试用，实际使用时请删除
-	return llvm::make_unique<FunctionAST>(std::move(Proto), std::move(llvm::make_unique<VariableExprAST>("ats")));
-	//return nullptr;
+  if (auto S = ParseStatement())
+    return llvm::make_unique<FunctionAST>(std::move(Proto), std::move());
+  // TODO: 这个返回只是测试用，实际使用时请删除
+  return llvm::make_unique<FunctionAST>(
+      std::move(Proto), std::move(llvm::make_unique<VariableExprAST>("ats")));
+  // return nullptr;
 }
 
 void HandleDefinition() {
-	if (ParseDefinition()) {
-		fprintf(stderr, "Parsed a function definition.\n");
-	}
-	else {
-		// Skip token for error recovery.
-		getNextToken();
-	}
+  if (ParseDefinition()) {
+    fprintf(stderr, "Parsed a function definition.\n");
+  } else {
+    // Skip token for error recovery.
+    getNextToken();
+  }
 }
