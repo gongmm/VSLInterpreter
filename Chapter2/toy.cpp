8e5856ceee4162196d439c0429f56c437f04e49e
2016302580101 <31274146+2016302580101@users.noreply.github.com>
2018-12-19 22:28:38 +0800

    显示结果加生成.o文件

--- Chapter2/toy.cpp
+++ Chapter2/toy.cpp
@@ -80,6 +80,71 @@ int main() {
   // Print out all of the generated code.
   TheModule->print(errs(), nullptr);
 
+
+  /*auto H = TheJIT->addModule(std::move(TheModule));
+  auto ExprSymbol = TheJIT->findSymbol("main");
+  assert(ExprSymbol && "Function not found");
+  double(*FP)() = (double(*)())(intptr_t)cantFail(ExprSymbol.getAddress());
+  fprintf(stderr, "Evaluated to %f\n", FP());*/
+  /***********************************************²âÊÔÊä³ö.oÎÄ¼þ*************************************/
+  // Initialize the target registry etc.
+
+  auto TargetTriple = sys::getDefaultTargetTriple();
+  TheModule->setTargetTriple(TargetTriple);
+
+  std::string Error;
+  auto Target = TargetRegistry::lookupTarget(TargetTriple, Error);
+
+  // Print an error and exit if we couldn't find the requested target.
+  // This generally occurs if we've forgotten to initialise the
+  // TargetRegistry or we have a bogus target triple.
+  if (!Target) {
+	  errs() << Error;
+	  return 1;
+  }
+
+  auto CPU = "generic";
+  auto Features = "";
+
+  TargetOptions opt;
+  auto RM = Optional<Reloc::Model>();
+  auto TheTargetMachine =
+	  Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
+
+  TheModule->setDataLayout(TheTargetMachine->createDataLayout());
+
+  auto Filename = "output.o";
+  std::error_code EC;
+  raw_fd_ostream dest(Filename, EC, sys::fs::F_None);
+
+  if (EC) {
+	  errs() << "Could not open file: " << EC.message();
+	  return 1;
+  }
+
+  legacy::PassManager pass;
+  auto FileType = TargetMachine::CGFT_ObjectFile;
+
+  if (TheTargetMachine->addPassesToEmitFile(pass, dest, FileType)) {
+	  errs() << "TheTargetMachine can't emit a file of this type";
+	  return 1;
+  }
+
+  pass.run(*TheModule);
+  dest.flush();
+
+  outs() << "Wrote " << Filename << "\n";
+
+
+  /***********************************************************************************/
+  auto H = TheJIT->addModule(std::move(TheModule));
+  auto ExprSymbol = TheJIT->findSymbol("main");
+  assert(ExprSymbol && "Function not found");
+  double(*FP)() = (double(*)())(intptr_t)cantFail(ExprSymbol.getAddress());
+  fprintf(stderr, "Evaluated to %f\n", FP());
+  // Delete the anonymous expression module from the JIT.
+  TheJIT->removeModule(H);
+
   // Initialize the target registry etc.
   //  InitializeAllTargetInfos();
   //  InitializeAllTargets();
